150cd4246cd336f3bdfa3bfb47c4e95a149b6271
Jared Davidson (Archetapp) <archetapp@gmail.com>
2021-03-15 18:22:35 -0600

    Initial Commit

---
+++ Shared/Clubhouse2App.swift
@@ -0,0 +1,17 @@
+//
+//  Clubhouse2App.swift
+//  Shared
+//
+//  Created by Jared on 3/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Clubhouse2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
